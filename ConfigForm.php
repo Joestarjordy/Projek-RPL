51325e36b60a13b0225147354774923e7b1ae38f
Joestarjordy <buana779@gmail.com>
2022-11-29 23:20:02 +0700

    mantap RPL

---
+++ ConfigForm.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "project rpl");
+
+function proposal()
+{
+    global $conn;
+
+    $nama = $_POST["nama"];
+    $nim = $_POST["nim"];
+    $topikskripsi = $_POST["topikskripsi"];
+    $calonpembimbing = $_POST["calonpembimbing"];
+    $calonpembimbing2 = $_POST["calonpembimbing2"];
+    $berkasdokumen = $_POST["berkasdokumen"];
+
+    // cek nim
+    $result = mysqli_query($conn, "SELECT nim FROM mahasiswa WHERE nim = '$nim'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script> alert ('Nim sudah terdaftar, pastikan nim terlebih dahulu') </script>";
+        return false;
+    }
+
+    // cek topik skripsi
+    $result = mysqli_query($conn, "SELECT topikskripsi FROM mahasiswa WHERE topikskripsi = '$topikskripsi'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script> alert ('Topik skripsi sudah terdaftar') </script>";
+        return false;
+    }
+
+    // menambahkan mahasiswa baru ke database
+    mysqli_query($conn, "INSERT INTO mahasiswa VALUES('$nama', '$nim', '$topikskripsi', '$calonpembimbing', '$calonpembimbing2', 
+    '$berkasdokumen')");
+    return mysqli_affected_rows($conn);
+}
---
+++ UnduhBerkasPage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.min.css">
+  <title> Project Akhir </title>
+</head>
+
+<body id="page-top">
+  <script src="bootstrap-5.0.2-dist/js/bootstrap.min.js"> </script>
+  <br><br><br><br>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-secondary fixed-top" id="mainNav">
+    <div class="container">
+      <a class="nav-item" href="#page-top">
+        <img src="gambar/filkom_ub.png" width="170" height="65" alt="filkom_ub">
+      </a>
+
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="text-white nav-link active fs-5 p-3" aria-current="page" href="UnduhBerkasPage.php">Unduh Berkas</a>
+          </li>
+          <li class="nav-item">
+            <a class="text-white nav-link active fs-5 p-3" aria-current="page" href="PraProposalPage.php">PraProposal</a>
+          </li>
+          <li class="nav-item">
+            <a class="text-white nav-link active fs-5 p-3" aria-current="page" href="ProposalPage.php">Proposal</a>
+          </li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</body>
+
+</html>
