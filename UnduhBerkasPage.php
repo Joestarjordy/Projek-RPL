c3bc01948f05335f088189a378a048d35409a43a
Joestarjordy <buana779@gmail.com>
2022-12-02 23:41:15 +0700

    add new change

--- UnduhBerkasPage.php
+++ UnduhBerkasPage.php
@@ -2,52 +2,52 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.min.css">
-  <title> Project Akhir </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <title> Project Akhir </title>
 </head>
 
 <body id="page-top">
-  <script src="bootstrap-5.0.2-dist/js/bootstrap.min.js"> </script>
-  <br><br><br><br>
+    <script src="bootstrap-5.0.2-dist/js/bootstrap.min.js"> </script>
+    <br><br><br><br>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-secondary fixed-top" id="mainNav">
-    <div class="container">
-      <a class="nav-item" href="ProposalPage.php">
-        <img src="gambar/filkom_ub.png" width="170" height="65" alt="filkom_ub">
-      </a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary fixed-top" id="mainNav">
+        <div class="container">
+            <a class="nav-item" href="ProposalPage.php">
+                <img src="gambar/filkom_ub.png" width="170" height="65" alt="filkom_ub">
+            </a>
 
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="text-white nav-link active fs-5 p-3" aria-current="page" href="UnduhBerkasPage.php">Unduh Berkas</a>
-          </li>
-          <li class="nav-item">
-            <a class="text-dark nav-link active fs-5 p-3" aria-current="page" href="PraProposalPage.php">PraProposal</a>
-          </li>
-          <li class="nav-item">
-            <a class="text-dark nav-link active fs-5 p-3" aria-current="page" href="ProsesVerifikasi.php">Proposal</a>
-          </li>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="text-white nav-link active fs-5 p-3" aria-current="page" href="UnduhBerkasPage.php">Unduh Berkas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="text-dark nav-link active fs-5 p-3" aria-current="page" href="PraProposalPage.php">PraProposal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="text-dark nav-link active fs-5 p-3" aria-current="page" href="ProsesVerifikasi.php">Proposal</a>
+                    </li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
-  <footer class="text-center text-lg-start text-white bg-secondary">
-    <div class="text-center p-3">
-      © 2022 Copyright:
-      <a class="text-white" href="https://www.instagram.com/">Jordy Cahya Buana</a>
-    </div>
-  </footer>
+    <footer class="text-center text-lg-start text-white bg-secondary">
+        <div class="text-center p-3">
+            © 2022 Copyright:
+            <a class="text-white" href="https://www.instagram.com/">Jordy Cahya Buana</a>
+        </div>
+    </footer>
 
 </body>
 
